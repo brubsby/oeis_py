21832ee21f52dc6d26fea07eb0cd700ecde497a4
brubsby <brubsbybrubsby@gmail.com>
2024-06-03 13:51:02 -0400

    wsl support for yafu.py

--- modules/yafu.py
+++ modules/yafu.py
@@ -4,6 +4,7 @@ import logging
 import math
 import os
 import shutil
+import signal
 import subprocess
 import time
 import uuid
@@ -13,12 +14,15 @@ from modules import factordb
 import gmpy2
 
 logger = logging.getLogger("yafu")
+__yafu_dir = "C:/Software/yafu-master/" \
+    if os.name == 'nt' else os.path.join(os.path.expanduser("~"), "yafu")
+__yafu_bin = "yafu-x64-gc.exe" if os.name == 'nt' else "yafu-wsl-icc-static"
 
 
 def factor(expr, stop_after_one=False, report_to_factordb=True, threads=1, work=None, pretest=None):
     str_expression = str(expr)
     text = f"{len(str_expression)} char expression: {str_expression[:48] + '...' + str_expression[-48:]}" if len(
-        str_expression) > 100 else expr
+        str_expression) > 200 else expr
     header_log_message_parts = ["Running yafu"]
     if work:
         header_log_message_parts.append(f"from t{work}")
@@ -31,7 +35,6 @@ def factor(expr, stop_after_one=False, report_to_factordb=True, threads=1, work=
     str_expr = f"factor({str(expr)})"
     start_time = time.time()
     this_uuid = uuid.uuid4()
-    yafu_dir = "C:/Software/yafu-master/"
     dirpath = os.path.join("..", "data", "temp", str(this_uuid))
     filename = f"temp-{this_uuid}.dat"
     temp_filepath = os.path.join(dirpath, filename)
@@ -40,12 +43,12 @@ def factor(expr, stop_after_one=False, report_to_factordb=True, threads=1, work=
         os.makedirs(dirpath, exist_ok=True)
         # copy the yafu.ini, so that we can use it from the throwaway temp dir without polluting the yafu directory with
         # logs and restart points
-        shutil.copy(os.path.join(yafu_dir, "yafu.ini"), os.path.join(dirpath, "yafu.ini"))
+        shutil.copy(os.path.join(__yafu_dir, "yafu.ini"), os.path.join(dirpath, "yafu.ini"))
         with open(temp_filepath, "w") as temp:
             temp.write(f"{str_expr}\n")
         factors_filename = "factors.out"
         popen_arglist = [
-            os.path.join(yafu_dir, "yafu-x64-gc.exe"), "-of", factors_filename, "-v"
+            os.path.join(__yafu_dir, __yafu_bin), "-of", factors_filename, "-v"
         ]
         if stop_after_one:
             popen_arglist.append("-one")
@@ -84,12 +87,12 @@ def factor(expr, stop_after_one=False, report_to_factordb=True, threads=1, work=
                 elapsed = time.time() - start_time
                 # if elapsed > 10:  # report factors to factordb if it took more than 10 seconds in yafu
                 # always report factors if yafu was called and found a factor
-                if report_to_factordb and len(factors) > 1:
+                if report_to_factordb and len(factors) > 1 and elapsed > 5:
                     logger.info(f"yafu found factors: {factors}")
-                    factordb.report(expr, factors)
+                    factordb.report({expr: factors})
                     logger.info(f"yafu reported factors for expression: {expr}")
                 logger.info(f"yafu ran on {expr} for {elapsed:.02f} seconds")
-                return [gmpy2.mpz(factor) for factor in factors]
+                return [gmpy2.mpz(factor) for factor in sorted(factors)]
         except FileNotFoundError as e:
             time_elapsed = time.time() - start_time
             if pretest:
@@ -100,6 +103,67 @@ def factor(expr, stop_after_one=False, report_to_factordb=True, threads=1, work=
                     raise e
                 logger.info(f"yafu found no factors during pretest on: {expr}")
                 return [expr]
+    except Exception as e:
+        logger.error(e)
+        raise e
+    finally:
+        if proc:
+            proc.kill()
+        # cleanup temp dir
+        shutil.rmtree(dirpath, ignore_errors=True)
+
+
+def timed_factor(expr, time, threads=1):
+    str_expression = str(expr)
+    text = f"{len(str_expression)} char expression: {str_expression[:48] + '...' + str_expression[-48:]}" if len(
+        str_expression) > 200 else expr
+    logger.info(f"Running yafu for {text} for {time} seconds and stopping")
+    str_expr = f"factor({str(expr)})"
+    start_time = time.time()
+    this_uuid = uuid.uuid4()
+    dirpath = os.path.join("..", "data", "temp", str(this_uuid))
+    filename = f"temp-{this_uuid}.dat"
+    temp_filepath = os.path.join(dirpath, filename)
+    proc = None
+    try:
+        os.makedirs(dirpath, exist_ok=True)
+        # copy the yafu.ini, so that we can use it from the throwaway temp dir without polluting the yafu directory with
+        # logs and restart points
+        shutil.copy(os.path.join(__yafu_dir, "yafu.ini"), os.path.join(dirpath, "yafu.ini"))
+        with open(temp_filepath, "w") as temp:
+            temp.write(f"{str_expr}\n")
+        factors_filename = "factors.out"
+        popen_arglist = [
+            os.path.join(__yafu_dir, __yafu_bin), "-of", factors_filename, "-v"
+        ]
+        proc = subprocess.Popen(popen_arglist,
+                                stdout=subprocess.PIPE, stderr=subprocess.STDOUT, stdin=subprocess.PIPE,
+                                universal_newlines=True, cwd=dirpath, bufsize=1)
+        print(f"{str_expr}\n", file=proc.stdin, flush=True)
+        proc.stdin.close()
+        for i, line in enumerate(proc.stdout):
+            logger.debug(line[:-1])
+        proc.send_signal(signal.SIGINT)
+        proc.wait()
+        try:
+            with open(os.path.join(dirpath, factors_filename), "r") as factors_file:
+                lines = factors_file.readlines()
+                assert len(lines) == 1, "Should only be one line in factor output file per yafu invocation"
+                factors = lines[0].split("/")  # [2, 3^5, ...]
+                factors = [term.split("^") for term in factors]  # [[2], [3, 5], ...]
+                factors = [[gmpy2.mpz(term[0])] if len(term) == 1 else [gmpy2.mpz(term[0])] * int(term[1]) for term in factors[1:]]  # [[2], [3, 3, 3, 3, 3], ...]
+                factors = list(itertools.chain.from_iterable(factors))
+
+                # yafu doesn't give the remaining cofactor so we have to calculate it
+                # (worry about evaluating math expressions later)
+                assert type(expr) in [int, gmpy2.mpz]
+                factors.append(functools.reduce(lambda comp, div: comp // div, factors, expr))
+                elapsed = time.time() - start_time
+                logger.info(f"yafu ran on {expr} for {elapsed:.02f} seconds")
+                return [gmpy2.mpz(factor) for factor in sorted(factors)]
+        except FileNotFoundError as e:
+            time_elapsed = time.time() - start_time
+            logger.info(f"yafu ran on {expr} for {elapsed:.02f} seconds and didn't find any factors")
     except Exception as e:
         logger.error(e)
         raise e
