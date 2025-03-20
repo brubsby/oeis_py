5d87d1526dd2e5750e6fb0d8b5ca80bef6e6b0e8
brubsby <brubsbybrubsby@gmail.com>
2025-03-20 16:06:45 -0400

    change yafu.py to treat carriage returns correctly

--- modules/yafu.py
+++ modules/yafu.py
@@ -1,4 +1,5 @@
 import functools
+import io
 import itertools
 import logging
 import math
@@ -14,6 +15,14 @@ from modules import factordb, config
 import gmpy2
 
 logger = logging.getLogger("yafu")
+
+# magic to get yafu's logger to treat carriage returns correctly
+logger.propagate = False
+handler = logging.StreamHandler()
+handler.terminator = ""
+logger.addHandler(handler)
+
+
 __yafu_dir = config.get_yafu_dir()
 __yafu_bin = config.get_yafu_bin()
 
@@ -30,7 +39,7 @@ def factor(expr, stop_after_one=False, report_to_factordb=True, threads=1, work=
     if stop_after_one:
         header_log_message_parts.append("and stopping after one factor")
     header_log_message_parts.append(f"for {text}")
-    logger.info(" ".join(header_log_message_parts))
+    logger.info(" ".join(header_log_message_parts) + "\n")
     str_expr = f"factor({str(expr)})"
     start_time = time.time()
     this_uuid = uuid.uuid4()
@@ -60,19 +69,12 @@ def factor(expr, stop_after_one=False, report_to_factordb=True, threads=1, work=
         if pretest:
             popen_arglist.append("-pretest")
             popen_arglist.append(str(pretest))
-        proc = subprocess.Popen(popen_arglist,
-                                stdout=subprocess.PIPE, stderr=subprocess.STDOUT, stdin=subprocess.PIPE,
-                                universal_newlines=True, cwd=dirpath, encoding="UTF-16", bufsize=1)
-        print(f"{str_expr}\n", file=proc.stdin, flush=True)
+        proc = subprocess.Popen(popen_arglist, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, stdin=subprocess.PIPE, cwd=dirpath, bufsize=1)
+        proc.stdin.write(f"{str_expr}\n".encode())
+        proc.stdin.flush()
         proc.stdin.close()
-        try:
-            for i, line in enumerate(proc.stdout):
-                logger.debug(line[:-1])
-        except UnicodeDecodeError as e:
-            logger.exception(e)
-            print("proc.stdout.read:")
-            print(proc.stdout.read())
-            raise e
+        for line in io.TextIOWrapper(proc.stdout, encoding="utf-8", newline=''):
+            logger.debug(line)
         proc.wait()
         try:
             with open(os.path.join(dirpath, factors_filename), "r") as factors_file:
@@ -93,10 +95,10 @@ def factor(expr, stop_after_one=False, report_to_factordb=True, threads=1, work=
                 # if elapsed > 10:  # report factors to factordb if it took more than 10 seconds in yafu
                 # always report factors if yafu was called and found a factor
                 if report_to_factordb and len(factors) > 1:  # and elapsed > 0.5:
-                    logger.info(f"yafu found factors: {factors}")
+                    logger.info(f"yafu found factors: {factors}\n")
                     factordb.report({expr: factors})
-                    logger.info(f"yafu reported factors for expression: {expr}")
-                logger.info(f"yafu ran on {expr} for {elapsed:.02f} seconds")
+                    logger.info(f"yafu reported factors for expression: {expr}\n")
+                logger.info(f"yafu ran on {expr} for {elapsed:.02f} seconds\n")
                 return [gmpy2.mpz(factor) for factor in sorted(factors)]
         except FileNotFoundError as e:
             time_elapsed = time.time() - start_time
@@ -106,7 +108,7 @@ def factor(expr, stop_after_one=False, report_to_factordb=True, threads=1, work=
                 # perhaps from msieve
                 if pretest > 30 and time_elapsed < 3:
                     raise e
-                logger.info(f"yafu found no factors during pretest on: {expr}")
+                logger.info(f"yafu found no factors during pretest on: {expr}\n")
                 return [expr]
     except Exception as e:
         logger.error(e)
@@ -122,7 +124,7 @@ def timed_factor(expr, time, threads=1):
     str_expression = str(expr)
     text = f"{len(str_expression)} char expression: {str_expression[:48] + '...' + str_expression[-48:]}" if len(
         str_expression) > 500 else expr
-    logger.info(f"Running yafu for {text} for {time} seconds and stopping")
+    logger.info(f"Running yafu for {text} for {time} seconds and stopping\n")
     str_expr = f"factor({str(expr)})"
     start_time = time.time()
     this_uuid = uuid.uuid4()
@@ -147,7 +149,7 @@ def timed_factor(expr, time, threads=1):
         print(f"{str_expr}\n", file=proc.stdin, flush=True)
         proc.stdin.close()
         for i, line in enumerate(proc.stdout):
-            logger.debug(line[:-1])
+            logger.debug(line)
         proc.send_signal(signal.SIGINT)
         proc.wait()
         try:
@@ -164,11 +166,11 @@ def timed_factor(expr, time, threads=1):
                 assert type(expr) in [int, gmpy2.mpz]
                 factors.append(functools.reduce(lambda comp, div: comp // div, factors, expr))
                 elapsed = time.time() - start_time
-                logger.info(f"yafu ran on {expr} for {elapsed:.02f} seconds")
+                logger.info(f"yafu ran on {expr} for {elapsed:.02f} seconds\n")
                 return [gmpy2.mpz(factor) for factor in sorted(factors)]
         except FileNotFoundError as e:
             time_elapsed = time.time() - start_time
-            logger.info(f"yafu ran on {expr} for {elapsed:.02f} seconds and didn't find any factors")
+            logger.info(f"yafu ran on {expr} for {elapsed:.02f} seconds and didn't find any factors\n")
     except Exception as e:
         logger.error(e)
         raise e
@@ -182,34 +184,4 @@ def timed_factor(expr, time, threads=1):
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     print(factor("fib(1000)"))
-
-
-# b1 level and number of curves yafu would suggest
-def get_b1_curves(start_work, end_work):
-    assert end_work - start_work > 0, "end_work must be greater than start_work"
-    if end_work <= 15:
-        return 2000, 30
-    if start_work >= 15 and end_work == 20:
-        return 11000, 72
-    if start_work >= 20 and end_work == 25:
-        return 50000, 204
-    if start_work >= 25 and end_work == 30:
-        return 250000, 403
-    if start_work >= 30 and end_work == 35:
-        return 1000000, 826
-    if start_work >= 35 and end_work == 40:
-        return 3000000, 2105
-    if start_work >= 40 and end_work == 45:
-        return 11000000, 3961
-    if start_work >= 45 and end_work == 50:
-        return 43000000, 6419
-    if start_work >= 50 and end_work == 55:
-        return 110000000, 15015
-    if start_work >= 55 and end_work == 60:
-        return 260000000, 35303
-    if start_work >= 60 and end_work == 65:
-        return 860000000, 69407
-    if start_work >= 65:
-        return 25000000000, 300000
-    assert False, f"unsupported start ({start_work}) and end work ({end_work})"
 
